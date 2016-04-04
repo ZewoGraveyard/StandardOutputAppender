491efef840841f1138b656028ec2a3d363c71b74
Scott Byrns <contact@scottbyrns.com>
2016-04-03 20:44:37 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "CrateIO",
+    name: "STDOutAppender",
     dependencies: [
         .Package(url: "https://github.com/scottbyrns/Log.git", majorVersion: 0, minor: 4)
     ]
