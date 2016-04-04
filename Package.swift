eb3d9f350ee2c3f2080d069afa77cc2957a62087
Scott Byrns <contact@scottbyrns.com>
2016-04-03 21:16:24 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "STDOutAppender",
+    name: "StandardOutputAppender",
     dependencies: [
         .Package(url: "https://github.com/Zewo/Log.git", majorVersion: 0, minor: 4)
     ]
