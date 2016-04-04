c700fdd6f8f1ddbb96adf3e3574acc31f1b1b777
Scott Byrns <contact@scottbyrns.com>
2016-04-03 23:28:46 -0600

    Hanging comma for dependencies list

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "StandardOutputAppender",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/Log.git", majorVersion: 0, minor: 4)
+        .Package(url: "https://github.com/Zewo/Log.git", majorVersion: 0, minor: 4),
     ]
 )
